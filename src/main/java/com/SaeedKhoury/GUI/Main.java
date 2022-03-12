8d209e7e51d1375e7bec50255a5076bc0635745b
Ahmad-Jber <73906694+Ahmad-Jber@users.noreply.github.com>
2022-03-11 23:34:33 -0800

    20th-commit

--- src/main/java/com/SaeedKhoury/GUI/Main.java
+++ src/main/java/com/SaeedKhoury/GUI/Main.java
@@ -1,11 +1,4 @@
 package com.SaeedKhoury.GUI;
-
-import com.SaeedKhoury.DBCaT.Super;
-
-import java.sql.*;
-
-import static com.SaeedKhoury.DBCaT.Super.connection;
-
 public class Main {
     public static void main(String[] args){
         new Sign_in().view();
